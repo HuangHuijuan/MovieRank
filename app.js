0f3dc27bb08ca1d18e006e11612d7c2542a57f73
yuxiao zhang <yuxiaozhang@yuxiaos-MacBook-Pro.local>
2017-12-02 22:12:33 -0500

    modified:   .DS_Store 	new file:   .env 	modified:   app.js 	new file:   controllers/.DS_Store 	modified:   package.json

--- app.js
+++ app.js
@@ -6,8 +6,25 @@ const registerController = require('./controllers/register.js');
 
 const loginController = require('./controllers/login.js');
 
+const db_accessor = require('./controllers/db_accessor.js');
+
+const recommand = require('./controllers/recommand.js');
+
+const bodyParser     =        require("body-parser");
+
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
+});
+
+app.post('/recommand', function(req, res){
+  var param = req.body.username;
+  console.log(param);
+  recommand._credence_recommand(param, (result) => res.json(result));
 });
 
 const server = app.listen(3001, function () {
@@ -16,6 +33,8 @@ const server = app.listen(3001, function () {
 
   console.log('Example app listening at http://%s:%s', host, port);
 });
+
+
 
 //registerController.register();
 
@@ -34,6 +53,10 @@ const server = app.listen(3001, function () {
 // db_accessor._select(select_query, res => {
 // 	console.log(res);
 // })
+
+const userid = 2;
+
+recommand._credence_recommand(userid, (result) => console.log(result));
 
 
 loginController.login();
