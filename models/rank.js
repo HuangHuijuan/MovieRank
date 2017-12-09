f61257fb8d8f6381abf9f855a59c2e9abd52a4b4
Huijuan <huijuan.huang@yale.edu>
2017-12-08 23:48:00 -0500

    fix bugs of search and rank api

--- models/rank.js
+++ models/rank.js
@@ -2,19 +2,20 @@ const db_accessor = require('./db_accessor.js');
 
 function rank(userid, page, callback)
 {
-	let start = 0;
-	let end = 10;
-	if (page !== undefined) {
-		start = (page - 1) * 10;
-		end = start + 10;
-	}
+	
 
-	movieModel.rankMovieWithUID(userid, end, res => {
+	movieModel.rankMovieWithUID(userid, res => {
 		const len = res.length;
 		const numOfPages = Math.ceil(len / 10);
+		let start = 0;
+		let end = 10;
+		if (page !== undefined) {
+			start = (page - 1) * 10;
+			end = Math.min(start + 10, len);
+		}
 		callback({
 			numOfPages: numOfPages,
-			movies: res.slice(start);
+			movies: res.slice(start, end);
 		});
 	});
 }
